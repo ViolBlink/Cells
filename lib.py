094c501bd7d8ec527c5d17e4861a2a6e05aacce5
Khazret Meretukov <violblink@gmail.com>
2022-10-09 12:14:54 +0300

    Исправил некоторые ошибки

--- lib.py
+++ lib.py
@@ -2,19 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
+fig = plt.figure(figsize=(8, 8),dpi=100)
+ax = fig.add_subplot(111)
+
 # Считываем парамтры
 with open("Parametres.json") as q:
     f = json.load(q)
 
-f = f["GeneralParametrs"]
+file_Gen = f["GeneralParametrs"]
+file_Normal = f["Normal"]
 
-a = float(f[0]["a"])  # Размер диогонали
-x_min = float(f[0]["x_min"]) # Минимальное значение х для окна
-y_min = float(f[0]["y_min"]) # Максимальное значение х для окна
-y_max = float(f[0]["y_max"]) # Максимальное значение y для окна
-x_max = float(f[0]["x_max"]) # Минимальное значение y для окна
-Nx = f[0]["Nx"] # Число центров по x
-Ny = f[0]["Ny"] # Число центров по y
+a = float(file_Gen[0]["a"])  # Размер диогонали
+x_min = float(file_Gen[0]["x_min"]) # Минимальное значение х для окна
+y_min = float(file_Gen[0]["y_min"]) # Максимальное значение х для окна
+y_max = float(file_Gen[0]["y_max"]) # Максимальное значение y для окна
+x_max = float(file_Gen[0]["x_max"]) # Минимальное значение y для окна
+Nx = file_Gen[0]["Nx"] # Число центров по x
+Ny = file_Gen[0]["Ny"] # Число центров по y
+
+mean = [float(file_Normal[0]["x0"]), float(file_Normal[0]["y0"])] # Матрица среднего
+
+f = file_Normal[0]["Cov"].split("\n")
+f1 = f[0].split(" ")
+f2 = f[1].split(" ")
+
+Cov = [[float(f1[0]), float(f1[1])], [float(f2[0]), float(f2[1])]] # Матрица ковариации
+
+
+Field = [[0] * Nx for _ in range(Ny)] # Поля для заполнения притяжения
 
 hx = float(x_max - x_min)/Nx
 hy = float(y_max - y_min)/Ny
@@ -32,6 +47,7 @@ def AddCentres(ax):
     ax.axis('equal')
 
 def Closest(x, y):
+    """Функция, находящая ближайшую ячейку для точки (x, y), возвращает центр и индексы ячейки"""
 
     vx = X[0]
     vy = Y[0]
@@ -51,10 +67,92 @@ def Closest(x, y):
 
     return np.array([[vx], [vy]]), [ix, iy]
     
-
 def leng(v1, v2):
+    """Функция, возвращающая длину между двумя векторами"""
     return np.sqrt((v1[0] - v2[0]) ** 2 + (v1[1] - v2[1]) ** 2)
 
+def Grav():
+    """"Функция, которая по заданому полю расположений возвращает графф смещений"""
+    fil = [[0] * Nx for _ in range(Ny)]
+    
+    for x in range(Nx):
+        for y in range(Ny):
+            pos = [x, y] # Рассматриваеммая точка
+            Position = [-1, -1]
+
+
+            if(Field[x][y]):
+
+                Pre_Possible_positions = [Move([x, y], [1, 0]), Move([x, y], [0, 1]), # Смещаемся в четыре стороны и создаем массив
+                Move([x, y], [-1, 0]), Move([x, y], [0, -1])]                         # в котором хранятся возможные точки
+
+                Possible_positions = []                                                             # Создаем массив фактических точек для смещения
+
+                for Tile in Pre_Possible_positions:                                                    # Заполняем массив
+                    if(Tile != [-1, -1]):
+                        Possible_positions.append(Tile)
+                                                                                                    # Находим ближайшую точку для смещения
+                l = 0
+
+                for Tile in Possible_positions:
+                    if(Tile == Possible_positions[0]):
+                        l = leng(pos, Tile)
+                        Position = Tile
+                    else:
+                        if(leng(pos, Tile) < l):
+                            l = leng(pos, Tile)
+                            Position = Tile
+
+            fil[x][y] = Position                                                                    # Создаем графф смещений
+    return fil
+
+def Move(start, direction):
+    """Возвращает ближайшую от start точку в направлении, указаным direction"""
+    x = start[0] + direction[0]
+    y = start[1] + direction[1]
+
+    while((x < Nx) and (x >= 0) and (y < Ny) and (y > 0)):
+        if((Field[x][y] != 0) and (np.abs(start[0] - x) > 1) and (np.abs(start[0] - y) > 1)): 
+            return [x, y]
+        x = x + direction[0]
+        y = y + direction[1]
+    return [-1, -1]
+
+def GTD(Graph, pos):
+    """Обходит граф"""
+    
+    if(pos == Graph[Graph[pos[0], pos[1]][0], Graph[pos[0], pos[1]][1]]):
+        return 
+
+    else:
+        GTD(Graph, Graph[pos[0], pos[1]])
+
+def Squares_Gen(len: int):
+
+    squares = []
+    i = 0
+
+    while (i < len):
+        x, y = np.random.multivariate_normal(mean, Cov).T
+        v0, n = Closest(x, y)
+        # v0 = [[x], [y]]
+        angle = np.pi/4
+        
+        if(Field[n[1]][n[0]] == 0):
+            flag = 1
+            Field[n[1]][n[0]] = 1
+
+        else: 
+            flag = 0
+        
+        if (flag == 1):
+            ax.plot(v0[0][0], v0[1][0], 'x', color='r')
+            ax.axis('equal')
+
+            squares.append(Square(np.array([[v0[0][0]], [v0[1][0]]]), angle))
+            i += 1
+
+    return squares
 class Square:
     def __init__(self, v0, angle: float) -> None:
         Rotate = np.array([[0, 1], 
@@ -73,8 +171,8 @@ class Square:
             v += v0
         pass
 
-    def Print(self, col, ax):
+    def Print(self, col, axe):
         for i in range(4):
             x1, y1 = [self.points[i][0][0], self.points[(i + 1) % 4][0][0]], [self.points[i][1][0], self.points[(i + 1) % 4][1][0]]
-            ax.plot(x1, y1, color=col)
+            axe.plot(x1, y1, color=col)
         pass
--- main.py
+++ main.py
@@ -7,61 +7,12 @@ import json
 fig1 = plt.figure(figsize=(8, 8),dpi=100)
 ax1 = fig1.add_subplot(111)
 
-fig2 = plt.figure(figsize=(8, 8),dpi=100)
-ax2 = fig2.add_subplot(111)
-
-# Считываем парамтры распределения
 with open("Parametres.json") as q:
     f = json.load(q)
-
-a = float(f["GeneralParametrs"][0]["a"])
-Nx = int(f["GeneralParametrs"][0]["Nx"])
-Ny = int(f["GeneralParametrs"][0]["Ny"])
 Amaunt = int(f["GeneralParametrs"][0]["Count"])
-
-Field = [[0] * Nx for _ in range(Ny)] # Поле для проверки пересечения
-
-mean = [float(f["Normal"][0]["x0"]), float(f["Normal"][0]["y0"])] # Матрица среднего
-
-f = f["Normal"][0]["Cov"].split("\n")
-f1 = f[0].split(" ")
-f2 = f[1].split(" ")
-
-Cov = [[float(f1[0]), float(f1[1])], [float(f2[0]), float(f2[1])]] # Матрица ковариации
-
 """ Конец считывания данных """
 
-
-def Squares_Gen(len: int):
-
-    squares = []
-    i = 0
-
-    while (i < len):
-        x, y = np.random.multivariate_normal(mean, Cov).T
-        v0, n = lib.Closest(x, y)
-        # v0 = [[x], [y]]
-        angle = np.pi/4
-        
-        if(Field[n[1]][n[0]] == 0):
-            flag = 1
-            Field[n[1]][n[0]] = 1
-
-        else: 
-            flag = 0
-        
-        if (flag == 1):
-            ax2.plot(v0[0][0], v0[1][0], 'x', color='r')
-            ax2.axis('equal')
-
-            squares.append(lib.Square(np.array([[v0[0][0]], [v0[1][0]]]), angle))
-            i += 1
-
-    return squares
-
-
-# lib.AddCentres(ax2)
-squares = Squares_Gen(Amaunt)
+squares = lib.Squares_Gen(Amaunt)
 
 
 for sq in squares:
